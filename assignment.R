9244af827227b856417fee965ec0b61fee04333d
mcarlo <tom.anichini@gmail.com>
2014-08-15 06:42:07 -0700

    Scaled down version

--- assignment.R
+++ assignment.R
@@ -24,6 +24,9 @@ availTest <- testing[, colnames(testing) %in% colnames(availTrain)]
 
 answers <- predict(modGBM, newdata = availTest)
 #B A B A A E D B A A B C B A E E A B B B
+#B A B A A E D B A A B C B A E E A B B B
+
+varImp(availRF)
 modRF
 modGBM
 
@@ -41,6 +44,39 @@ pml_write_files = function(x){
     write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
   }
 }
+
+sampleClasse <- createDataPartition(y=availTrain$classe,
+                                           p=0.2, list=FALSE)
+sampleC <- availTrain[sampleClasse,]
+sampleUser <- createDataPartition(y=sampleC$user_name,
+                                  p=0.25, list=FALSE)
+sampleU <- sampleC[sampleUser,]
+sampleRF <- train(classe ~ ., method="rf",data=sampleU[,c(1:10,55)],verbose=FALSE)
+sampleRF
+sampleRFa <- train(classe ~ ., method="rf",data=sampleU[,c(11:27,55)],verbose=FALSE)
+sampleRFa
+sampleRFb <- train(classe ~ ., method="rf",data=sampleU[,c(28:41,55)],verbose=FALSE)
+sampleRFb
+sampleRFc <- train(classe ~ ., method="rf",data=sampleU[,c(42:54,55)],verbose=FALSE)
+sampleRFc
+availRF <- train(classe ~ ., method="rf",data=availTrain[sampleClasse,c(1:10,55)],verbose=FALSE)
+availRF
+system.time()
+Overall
+num_window        100.0000
+roll_belt          31.1554
+yaw_belt           22.8421
+pitch_belt         22.7179
+total_accel_belt    7.0694
+gyros_belt_z        4.3541
+user_nameeurico     4.3255
+accel_belt_x        3.8776
+user_namejeremy     3.5353
+gyros_belt_y        2.1700
+gyros_belt_x        2.1581
+user_namecharles    0.9833
+user_namecarlitos   0.7608
+user_namepedro      0.0000
 
 #pml_write_files(answers)
 ls()
